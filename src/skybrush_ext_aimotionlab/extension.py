9d12f67ffb48bc1594ac155a48e08788090a66d1
AIMotionLab <aimotionlab.sztaki@gmail.com>
2023-04-24 13:27:41 +0200

    Added the functionality to send non-uav object pose to the drones.

--- src/skybrush_ext_aimotionlab/extension.py
+++ src/skybrush_ext_aimotionlab/extension.py
@@ -1,14 +1,18 @@
 from trio import sleep
-
+from flockwave.server.app import SkybrushServer
 from flockwave.server.ext.base import Extension
+from contextlib import ExitStack
+from functools import partial
+from flockwave.server.ext.motion_capture import MotionCaptureFrame
+from trio import sleep_forever
+from .handler import AiMotionMocapFrameHandler
 
 __all__ = ("ext_aimotionlab", )
 
 
 class ext_aimotionlab(Extension):
-    """Template for Skybrush Server extensions."""
-
-    async def run(self, app, configuration, logger):
+    """Extension that broadcasts the pose of non-UAV objects to the Crazyflie drones."""
+    async def run(self, app: "SkybrushServer", configuration, logger):
         """This function is called when the extension was loaded.
 
         The signature of this function is flexible; you may use zero, one, two
@@ -24,8 +28,45 @@ class ext_aimotionlab(Extension):
             logger: Python logger object that the extension may use. Also
                 available as ``self.log``.
         """
+        port = configuration.get("port")
+        channel = configuration.get("channel")
+        assert self.app is not None
+        signals = self.app.import_api("signals")
+        broadcast = self.app.import_api("crazyflie").broadcast
+
         self.log.info("The new extension is now running.")
         await sleep(0.5)
         self.log.warning(configuration.get("configWord"))
         await sleep(0.5)
         self.log.info("One second has passed.")
+
+        with ExitStack() as stack:
+
+            # create a dedicated mocap frame handler
+            frame_handler = AiMotionMocapFrameHandler(broadcast, port, channel)
+            # subscribe to the motion capture frame signal
+            stack.enter_context(
+                signals.use(
+                    {
+                        "motion_capture:frame": partial(
+                            self._on_motion_capture_frame_received,
+                            handler=frame_handler,
+                        )
+                    }
+                )
+            )
+            await sleep_forever()
+
+    def _on_motion_capture_frame_received(
+            self,
+            sender,
+            *,
+            frame: "MotionCaptureFrame",
+            handler: AiMotionMocapFrameHandler,
+    ) -> None:
+        handler.notify_frame(frame)
+
+
+
+
+
---
+++ src/skybrush_ext_aimotionlab/handler.py
@@ -0,0 +1,42 @@
+
+from typing import List, Tuple
+from flockwave.server.utils import chunks
+from flockwave.server.ext.motion_capture import MotionCaptureFrame
+from aiocflib.utils.quaternion import QuaternionXYZW
+from aiocflib.crazyflie.localization import (
+    GenericLocalizationCommand,
+    Localization,
+)
+
+
+class AiMotionMocapFrameHandler:
+
+    def __init__(self, broadcast, port, channel):
+        self._broadcast = broadcast
+        self._port = port
+        self._channel = channel
+
+    def notify_frame(self, frame: "MotionCaptureFrame"):
+        # Prefixes which we classify as non-UAV.
+        valid_prefixes = ['bu', 'hook', 'test']
+        poses: List[Tuple[int, Tuple[float, float, float], QuaternionXYZW]] = []
+        for item in frame.items:
+            for prefix in valid_prefixes:
+                if item.name.startswith(prefix):
+                    try:
+                        numeric_id = int(item.name[len(prefix):])
+                    except ValueError:
+                        continue
+                    if item.attitude is not None and item.position is not None:
+                        w, x, y, z = item.attitude
+                        poses.append((numeric_id, item.position, QuaternionXYZW(x, y, z, w)))
+        # print("Starting to broadcast the poses: " + str(poses))
+        for chunk in chunks(poses, 2):
+            packet = bytes(
+                [GenericLocalizationCommand.EXT_POSE_PACKED]
+            ) + Localization.encode_external_pose_packed(chunk)
+            self._broadcast(
+                self._port,
+                self._channel,
+                packet,
+            )
